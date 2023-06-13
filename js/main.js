7647b897ad1d5a196f8cfb65c81769c306200b54
Mahmoud Elarby <mahmoud112233446547@gmail.com>
2023-06-13 04:39:02 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+// let nums = document.querySelectorAll(".nums-page .nums .num");
+// let section = document.querySelector(".nums-page");
+
+// let started = false;
+
+// window.onscroll = function () {
+//     if (window.scrollY >= section.offsetTop) {
+//         if (!started) {
+//             nums.forEach((nums) => startCount(nums)); 
+//         }
+//         started = true;
+//     }
+// }
+
+// function startCount(el) {
+//     let goal = el.dataset.goal;
+//     let count = setInterval(() => {
+//         el.textContent++;
+//         if (el.textContent == goal) {
+//             clearInterval(count);
+//         }
+//     }, 2000 / goal);
+// }
+
+
